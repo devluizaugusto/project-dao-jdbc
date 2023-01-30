6c023ef09619cca27e69d1be8d51712842133d63
Luiz Augusto <dev.luiz.augusto@gmail.com>
2023-01-30 14:32:39 -0300

    Department FindAll in main class department

--- src/application/MainMenuDepartment.java
+++ src/application/MainMenuDepartment.java
@@ -10,47 +10,53 @@ import model.entities.Department;
 public class MainMenuDepartment {
 
 	public static void main(String[] args) {
-			Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
+
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+		char op;
+
+		do {
+			System.out.println("1 - DEPARTMENT FIND BY ID");
+			System.out.println("2 - DEPARTMENT FIND ALL");
+			System.out.println("3 - DEPARTMENT INSERT");
+			System.out.println("4 - DEPARTMENT UPDATE");
+			System.out.println("5 - DEPARTMENT DELETE");
+			System.out.println("6 - CLOSE");
+			System.out.print("CHOOSE AN OPTION: ");
+			int option = sc.nextInt();
+
+			System.out.println();
+
+			switch (option) {
+			case 1: {
+				System.out.print("ENTER WITH DEPARTMENT ID: ");
+				int id = sc.nextInt();
+
+				Department dep = departmentDao.findById(id);
+
+				System.out.println("-----------------------------------------------------");
+
+				System.out.println(dep);
+
+				break;
+			}
 			
-			DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
-			
-			char op;
-
-			do {
-				System.out.println("1 - DEPARTMENT FIND BY ID");
-				System.out.println("2 - DEPARTMENT FIND ALL");
-				System.out.println("3 - DEPARTMENT INSERT");
-				System.out.println("4 - DEPARTMENT UPDATE");
-				System.out.println("5 - DEPARTMENT DELETE");
-				System.out.println("6 - CLOSE");
-				System.out.print("CHOOSE AN OPTION: ");
-				int option = sc.nextInt();
-
-				System.out.println();
-
-				switch (option) {
-				case 1: {
-					System.out.print("ENTER WITH DEPARTMENT ID: ");
-					int id = sc.nextInt();
-
-					Department dep = departmentDao.findById(id);
-
-					System.out.println("-----------------------------------------------------");
-
-					System.out.println(dep);
-
-					break;
+			case 2: {
+				List<Department> list = departmentDao.findAll();
+				for (Department dep : list) {
+					System.out.println("\n" + dep);
 				}
-				}
-				System.out.println();
+			}
+		}
+			System.out.println();
 
-				System.out.print("DO YOU WANT TO RETURN TO THE MAIN MENU: ");
-				op = sc.next().charAt(0);
+			System.out.print("DO YOU WANT TO RETURN TO THE MAIN MENU: ");
+			op = sc.next().charAt(0);
 
-				System.out.println();
-			} while (op == 'y');
-			
-			sc.close();
+			System.out.println();
+		} while (op == 'y');
+
+		sc.close();
 	}
 }
-			
